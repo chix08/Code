f66a44df393b13e5d5da82c83bbbff47d4791e8c
Chirag <>
2019-06-23 12:34:23 +0530

    Changes Required For TLE

---
+++ EvenOddQueries.py
@@ -0,0 +1,34 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+
+test = int(input())
+while test:
+    test -= 1
+    n, q = map(int, (input().split()))
+    arr = list(map(int, (input().split())))
+    while q:
+        q -= 1
+        k, l, r = map(int, (input().split()))
+        l -= 1
+        even = 0
+        for i in range(l, r):
+            if arr[i] % 2 == 0:
+                even += 1
+        if k:
+            if (r - l - even):
+                b = gcd((r - l - even), (r - l))
+                print(int((r - l - even) / b), int((r - l) / b))
+            else:
+                print('0')
+
+        else:
+            if even:
+                b = gcd(even, (r - l))
+                print(int(even / b), int((r - l) / b))
+
+            else:
+                print('0')
+
