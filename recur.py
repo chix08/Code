66582a5d466cd281c98610ac961bdcd68718bcf3
Chirag <chirag.pagaria@blazeclan.com>
2019-07-08 16:09:37 +0530

    reccursion

---
+++ recur.py
@@ -0,0 +1,21 @@
+# def recur(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * recur(n-1)
+#
+#
+# a = recur(5)
+# print(a)
+#
+# def ar(arr):
+#     if len(arr) == 1:
+#         return arr
+#     else:
+#         arr.pop()
+#         return ar(arr)
+#
+#
+# a = [1, 2, 3, 4, 5, 6, 7]
+# print(ar(a))
+#
