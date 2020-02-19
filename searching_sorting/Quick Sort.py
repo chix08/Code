0f0f4539ffda5871e0d8908ef33377746644b383
Chirag <chirag.pagaria@blazeclan.com>
2020-02-19 11:30:56 +0530

    indentations changes

--- searching_sorting/Quick Sort.py
+++ searching_sorting/Quick Sort.py
@@ -1,8 +1,8 @@
 def quick_sort(array, low, high):
     if low < high:
         pi = partition(array, low, high)
-        quick_sort(array, low, pi-1)
-        quick_sort(array, pi+1, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
 
 
 def partition(array, low, high):
@@ -18,9 +18,10 @@ def partition(array, low, high):
     array[left], array[high] = array[high], array[left]
     return left
 
-array = [15,1,8,4,3,9,7,50,12,11]
+
+array = [15, 1, 8, 4, 3, 9, 7, 50, 12, 11]
 
 low = 0
 high = len(array) - 1
-na = quick_sort(array, low, high)
+quick_sort(array, low, high)
 print(array)
