d87a7710972c2e4224c9c26a965975cf3603444c
Chirag <chirag.pagaria@blazeclan.com>
2020-02-20 17:06:10 +0530

    post order

--- searching_sorting/binary_tree.py
+++ searching_sorting/binary_tree.py
@@ -66,8 +66,69 @@ class Node:
             n = n.right
             n.inorder()
 
+    def inOrder(self, root):
 
+        # Set current to root of binary tree
+        current = root
+        stack = []  # initialize stack
+        done = 0
 
+        while True:
+
+            # Reach the left most Node of the current Node
+            if current is not None:
+
+                # Place pointer to a tree node on the stack
+                # before traversing the node's left subtree
+                stack.append(current)
+
+                current = current.left
+
+                # BackTrack from the empty subtree and visit the Node
+            # at the top of the stack; however, if the stack is
+            # empty you are done
+            elif (stack):
+                current = stack.pop()
+                # print(current.data, end=" ")  # Python 3 printing
+
+                # We have visited the node and its left
+                # subtree. Now, it's right subtree's turn
+                current = current.right
+
+            else:
+                break
+
+    def postOrderIterative(root):
+
+        if root is None:
+            return
+
+            # Create two stacks
+        s1 = []
+        s2 = []
+
+        # Push root to first stack
+        s1.append(root)
+
+        # Run while first stack is not empty
+        while s1:
+
+            # Pop an item from s1 and
+            # append it to s2
+            node = s1.pop()
+            s2.append(node)
+
+            # Push left and right children of
+            # removed item to s1
+            if node.left:
+                s1.append(node.left)
+            if node.right:
+                s1.append(node.right)
+
+                # Print all elements of second stack
+        while s2:
+            node = s2.pop()
+            print node.data
 
 root = Node('D')
 root_l = Node('B')
@@ -84,7 +145,8 @@ test = Node('K')
 test2 = Node('S')
 leaf_root_l_r.left = test
 leaf_root_l_r.right = test2
-# post_seq = root.pre_order()
-# print(post_seq)
+post_seq = root.pre_order()
+print(post_seq)
 root.inorder()
 print (inorderseq)
+root.inOrder(root)
